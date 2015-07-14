79f50de93e1debe6899ff546b1c1c2b30c16dc00
Stephen Eyer <stephen@stepheneyer.com>
2015-07-13 17:37:02 -0700

    Added ability to save/download canvas

--- client/index.js
+++ client/index.js
@@ -26,11 +26,39 @@ if (Meteor.isClient) {
 });
 
   Template.filter.events({
-  'click .reactive-table tr': function (event) {
-    event.preventDefault();
-    var row = this;
-    var fullQuote = row.quote + ' -' + row.author;
-    console.log(fullQuote);
-  }
+    'click .reactive-table tr': function (event) {
+      event.preventDefault();
+      var row = this;
+      var fullQuote = row.quote + '\n' + ' -' + row.author;
+      console.log(fullQuote.length);
+      var canvas = new fabric.Canvas('c');
+      var img = new Image();
+      img.onload = function(){
+      canvas.setBackgroundImage(img.src, canvas.renderAll.bind(canvas), {
+            originX: 'left',
+            originY: 'top',
+            left: 0,
+            top: 0
+        });
+      };
+      img.src = "http://localhost:3000/birdseed_quotation_marks.png";
+      canvas.setHeight(220);
+      canvas.setWidth(440);
+      var text = new fabric.Text(fullQuote, { 
+        fontFamily: 'Arial',
+        fontSize: 16,
+        left: 90,
+        top: 100
+      });
+      canvas.add(text);
+    },
+      'click #saveBtn' : function (event) {
+      event.preventDefault();
+      var canvasSelect = document.getElementById("c");
+      var image = canvasSelect.toDataURL("image/png").replace("image/png", "image/octet-stream");
+      window.location.href = image;
+      // document.write('<img src="'+image+'"/>');
+      console.log(image);
+    }
 });
 }
